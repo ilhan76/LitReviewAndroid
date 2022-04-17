2de5e84fc3c19cfa1e8e9fc0cdee4d643df6a18c
kudashov <kudashov.ilya@gmail.com>
2022-04-17 14:00:55 +0300

    initial commit

---
+++ f-splash/src/main/java/com/litreview/f_splash/ui/splash/SplashFragment.kt
@@ -0,0 +1,12 @@
+package com.litreview.f_splash.ui.splash
+
+import androidx.fragment.app.Fragment
+import com.litreview.f_splash.R
+
+class SplashFragment : Fragment(R.layout.splash_fragment) {
+
+    companion object {
+        fun newInstance() = SplashFragment()
+    }
+
+}

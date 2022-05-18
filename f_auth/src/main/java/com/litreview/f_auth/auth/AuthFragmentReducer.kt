5f6286210ad087a21087eb0ab7dbee57833ca5e3
kudashov <kudashov.ilya@gmail.com>
2022-05-18 21:23:36 +0300

    Добавлен commandHolder

--- f_auth/src/main/java/com/litreview/f_auth/auth/AuthFragmentReducer.kt
+++ f_auth/src/main/java/com/litreview/f_auth/auth/AuthFragmentReducer.kt
@@ -1,14 +1,24 @@
-package com.litreview.f_auth
+package com.litreview.f_auth.auth
 
+import com.litreview.base.util.Command
 import com.litreview.base.util.EMPTY_STRING
 import ru.surfstudio.mvi.core.reducer.Reducer
-import com.litreview.f_auth.AuthFragmentEvent.*
+import com.litreview.f_auth.auth.AuthFragmentEvent.*
+import com.litreview.i_navigation.NavCommand
 import javax.inject.Inject
+import javax.inject.Singleton
 
-data class AuthState (
+data class AuthState(
     val email: String = EMPTY_STRING,
     val password: String = EMPTY_STRING
 )
+
+@Singleton
+class AuthCommandHolder @Inject constructor() {
+    val openScreen = Command<NavCommand>()
+    val showEmailValidationError = Command<Int>()
+    val showPasswordValidationError = Command<Int>()
+}
 
 class AuthFragmentReducer @Inject constructor() : Reducer<AuthFragmentEvent, AuthState> {
 

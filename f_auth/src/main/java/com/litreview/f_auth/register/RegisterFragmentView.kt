9ef9e3e5ddec9d73c57395edf3d924767cb99a00
kudashov <kudashov.ilya@gmail.com>
2022-05-22 15:48:39 +0300

    Исправлен краш и верстка

--- f_auth/src/main/java/com/litreview/f_auth/register/RegisterFragmentView.kt
+++ f_auth/src/main/java/com/litreview/f_auth/register/RegisterFragmentView.kt
@@ -1,6 +1,7 @@
 package com.litreview.f_auth.register
 
 import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import by.kirich1409.viewbindingdelegate.viewBinding
@@ -14,8 +15,8 @@ class RegisterFragmentView : Fragment(R.layout.fragment_register),
     override val viewModel by viewModels<RegisterViewModel>()
     private val vb by viewBinding(FragmentRegisterBinding::bind)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         initToolbar()
     }
 

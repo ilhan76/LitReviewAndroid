094f2de587f9200b142f0f9213f18b57ed2d644f
kudashov <kudashov.ilya@gmail.com>
2022-06-02 20:04:35 +0300

    Сетевой слой для экрана

---
+++ i_feed/src/main/java/com/litreview/i_feed/FeedApi.kt
@@ -0,0 +1,20 @@
+package com.litreview.i_feed
+
+import com.litreview.base.data.dto.BookDto
+import retrofit2.Response
+import retrofit2.http.GET
+import com.litreview.i_network.Urls.Books.BOOKS
+import com.litreview.i_network.Urls.Books.NEW_BOOKS
+import com.litreview.i_network.Urls.Books.BEST_BOOKS
+
+interface FeedApi {
+
+    @GET(BOOKS)
+    suspend fun getMyBooks(): Response<List<BookDto>>
+
+    @GET(NEW_BOOKS)
+    suspend fun getNewBooks(): Response<List<BookDto>>
+
+    @GET(BEST_BOOKS)
+    suspend fun getBestBooks(): Response<List<BookDto>>
+}
--- i_network/src/main/java/com/litreview/i_network/ServerUrls.kt
+++ i_network/src/main/java/com/litreview/i_network/ServerUrls.kt
@@ -18,4 +18,11 @@ object Urls {
         const val GET_BOOKS = "$PROFILE/books"
         const val ADD_BOOK = "$PROFILE/book"
     }
+
+    object Books {
+        const val BOOKS = "books"
+
+        const val BEST_BOOKS = "$BOOKS?rate=4"
+        const val NEW_BOOKS = "$BOOKS/news"
+    }
 }

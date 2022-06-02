6b84fbf2c8f38df391cb4340224543e591da9e01
kudashov <kudashov.ilya@gmail.com>
2022-06-02 19:37:34 +0300

    Обновлены модельки

--- base/src/main/java/com/litreview/base/data/domain/Book.kt
+++ base/src/main/java/com/litreview/base/data/domain/Book.kt
@@ -1,8 +1,9 @@
 package com.litreview.base.data.domain
 
 data class Book(
-    val authorId: Int,
     val title: String,
-    val image: String,
-    val rate: Double
+    val imageUrl: String,
+    val description: String,
+    val rate: Double,
+    val author: Author?
 )
---
+++ base/src/main/java/com/litreview/base/data/dto/BookDto.kt
@@ -0,0 +1,31 @@
+package com.litreview.base.data.dto
+
+import com.google.gson.annotations.SerializedName
+import com.litreview.base.data.Transformable
+import com.litreview.base.data.domain.Book
+
+data class BookDto(
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("title")
+    val title: String,
+    @SerializedName("image")
+    val imageUrl: String,
+    @SerializedName("description")
+    val description: String,
+    @SerializedName("rate")
+    val rate: Double,
+    @SerializedName("author")
+    val author: AuthorDto?
+) : Transformable<Book> {
+    override fun transform(): Book {
+        return Book(
+            title = title,
+            imageUrl = imageUrl,
+            description = description,
+            rate = rate,
+            author = author?.transform()
+        )
+    }
+
+}
